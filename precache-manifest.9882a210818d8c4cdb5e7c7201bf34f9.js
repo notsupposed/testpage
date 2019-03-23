02d57ab29e516582b40dcd0e0bdadf403010d42d
Deploy robot <not@exists.not>
2019-03-23 19:49:41 +0300

    Auto-generated commit

---
+++ precache-manifest.9882a210818d8c4cdb5e7c7201bf34f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ffd2bf4712d0e61edc73",
+    "url": "/static/js/main.4f371406.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "ffd2bf4712d0e61edc73",
+    "url": "/static/css/main.defe0ef9.chunk.css"
+  },
+  {
+    "revision": "2c707eb5678d8edef6a70aec4aab4de8",
+    "url": "/index.html"
+  }
+];
